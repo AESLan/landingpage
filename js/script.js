4bde73fff6c881788b1ad3789293c10776be3e0b
unknown <alexmusicalsa@gmail.com>
2022-10-07 14:00:40 -0300

    Slideshow

--- js/script.js
+++ js/script.js
@@ -23,6 +23,24 @@ $(document).ready(function () {
   });
 });
 
-$(function () {
-  $(".flex-image").colorbox();
-});
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("flex-item");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
