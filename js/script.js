65990ed788a836d618863c41a3054d35dfe48ea8
unknown <alexmusicalsa@gmail.com>
2022-10-07 23:05:32 -0300

    Adicionado formulário para contato

--- js/script.js
+++ js/script.js
@@ -23,6 +23,15 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $(".btn-contato").on("click", function () {
+    $(".formulario").toggle("slow");
+    $(".main-navigation").toggle("slow");
+  });
+});
+
+/**Slideshow de Imagens */
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -43,4 +52,30 @@ function showDivs(n) {
     x[i].style.display = "none";
   }
   x[slideIndex - 1].style.display = "block";
+}
+
+/**Envio de mensagem pelo whatsapp */
+
+function abrirWhatsapp() {
+  var nome = document.getElementById("nome").value;
+  var telefone = document.getElementById("telefone").value;
+  var email = document.getElementById("email").value;
+  var msg = document.getElementById("msg").value;
+  var url =
+    "https://wa.me/51995428272?text=" + // Seu numero
+    "*Formulário de Contato*" +
+    "%0a" + // Mensagem personalizada
+    "%0a" + // Quebra de linha
+    "*Nome*: " +
+    nome +
+    "%0a" + // Dados do formulário
+    "*Telefone*: " +
+    telefone +
+    "%0a" +
+    "*E-mail*: " +
+    email +
+    "%0a" +
+    "*Mensagem*: " +
+    msg;
+  window.open(url, "_blank").focus();
 }
