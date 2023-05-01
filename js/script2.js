840fc86b214ed808535a76d376f460a0b0e20188
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-05-01 11:20:48 -0300

    testando git commit -m testando git commit -m testando

--- js/script2.js
+++
@@ -1,9 +0,0 @@
-/*página2 */
-
-var drop = document.getElementsByClassName("drop");
-var dropdown = document.getElementsByClassName("dropdown");
-
-drop.addEventListener("mouseover", function() {
-  drop.classList.add("drop")
-  
-});
